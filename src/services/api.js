43ae3a982f9a3415a4732116871353d0b85f100a
Brendon Hudson <brendonhc@outlook.com>
2019-03-01 10:35:04 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Conexão com o backend em node
 const api = axios.create({
-    baseURL: 'http://192.168.1.111:3001/'
+    baseURL: 'https://aprovaja-monitorasummer-back.herokuapp.com/'
 })
 
 export default api;
