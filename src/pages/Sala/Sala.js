c691eb77ca900ca5536d22ac896f52100b48da5c
Leonardo Daher <leonardo.daher@usp.br>
2019-03-01 04:33:37 -0300

    Mostrar instrutores e filtrar por categoria

--- src/pages/Sala/Sala.js
+++ src/pages/Sala/Sala.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Form } from 'react-bootstrap';
+import { Card, Button, Row, Form } from 'react-bootstrap';
 
 import MenuSala from '../../components/MenuSala'
 import Footer from '../../components/Footer'
@@ -7,9 +7,23 @@ import Footer from '../../components/Footer'
 import './styles.css';
 
 // import api from '../../services/api';
-import { storagedTokenIsValid } from '../../services/token';
+import { storagedTokenIsValid, getStoragedToken } from '../../services/token';
+import api from '../../services/api';
 
 class Sala extends Component {
+
+  state = {
+    instrutores: []
+  }
+
+  handleChangeCategoria = event => {
+    const token = getStoragedToken();
+    api.get('instrutores/?categoria='+event.target.value+'&token='+token)
+      .then(response => {
+        const instrutores = response.data.data;
+        this.setState({ instrutores: instrutores });
+      });
+  }
 
   async componentDidMount() {
     const isValid = await storagedTokenIsValid()
@@ -17,23 +31,47 @@ class Sala extends Component {
       console.log('Loga aí, né mano!')
       this.props.history.push("/")
     }
+
+    const token = getStoragedToken();
+    const response = await api.get('instrutores/?token='+token);
+
+    await this.setState({instrutores: response.data.data});
   }
 
   render() {
+    const instList = this.state.instrutores.map((instrutor) => 
+      <Card className="m-4" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{instrutor.nome}</Card.Title>
+          <Card.Text>
+            {instrutor.descricao}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    )
+
     return (
       <div className="container-fluid">
         <Row>
           <MenuSala />
         </Row>
-        <Row className="justify-content-center align-items-center" style={{ minHeight: (window.innerHeight - 200) }}>
+        <Row className="justify-content-center align-items-center">
           <Form.Group className="container" controlId="exampleForm.ControlSelect1">
             <Form.Label>Categorias</Form.Label>
-            <Form.Control as="select">
+            <Form.Control onChange={this.handleChangeCategoria} as="select">
               <option>Tecnologia</option>
-              <option>Consultoria</option>
-              <option>Engenharia</option>
+              <option>Saúde</option>
+              <option>Administração</option>
+              <option>Recursos Humanos</option>
+              <option>Entrevistas</option>
+              <option>Dinâmicas</option>
             </Form.Control>
           </Form.Group>
+        </Row>
+        <Row className="justify-content-center">
+          {instList}
         </Row>
         <Row>
           <Footer />
